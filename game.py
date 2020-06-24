643a5460bf25d04219f918eacf1914982c478771
AnnoyCreations <67360630+AnnoyCreations@users.noreply.github.com>
2020-06-24 22:26:22 +0530

    Add files via upload

--- game.py
+++ game.py
@@ -11,15 +11,15 @@ time.sleep(4)
 
 os.system('clear')
 
-bar = "\033[1;33;40m\n_________________________________________________\n"
+bar = "\033[1;34;40m\n#########################################################\n"
 
 def name():
         os.system("toilet -f smblock --filter border:metal '~ ~ Mega Run ~ ~'")
-        print("--------------------------------,\n")
-        print("| Author    : Annoy Creations   |,\n")
-        print("| Telegrame : t.me/hackerrdt    |,\n")
-        print("| FaceBook  : Network Hacker    |,\n")
-        print("--------------------------------,\n")
+        print("\033[1;35;40m--------------------------------,\n")
+        print("\033[0;37;46m| Author    : Annoy Creations   |,\n")
+        print("\033[0;37;46m| Telegrame : t.me/hackerrdt    |,\n")
+        print("\033[0;37;46m| FaceBook  : Network Hacker    |,\n")
+        print("\033[1;35;40m--------------------------------,\n")
 print(name(),"\n")
 
 try:
@@ -27,7 +27,7 @@ try:
     auth = f.read()
     f.close
 except:
-    wr = str(input("\033[1;0;40mPaste Your Auth here :- "))
+    wr = str(input("\033[1;36;40mPaste Your Auth here :- "))
     f = open("auth.txt", "w")
     f.write(wr)
     f.close
@@ -41,7 +41,7 @@ try:
     url1 = f.read()
     f.close
 except:
-    wr = str(input("Paste Your URL here :- "))
+    wr = str(input("\033[1;36;40mPaste Your URL here :- "))
     f = open("url.txt", "w")
     f.write(wr)
     f.close
@@ -75,33 +75,33 @@ def main():
         resp = str(res)
         if resp == '<Response [204]>':
             print(bar)
-            print("\n\033[1;32;40m [#] No Data ... [#]")
+            print("\n\033[0;37;42m [#] No Data ... [#]")
             print(bar)
         elif resp == '<Response [200]>':
             print(bar)
-            print("\n\033[1;32;40m [#] $ You Won Data $... [#]")
+            print("\n\033[0;37;42m [#] $ You Won Data $... [#]")
             print(bar)
         else:
             print(bar)
-            print("\n\033[1;31;40m [#] Check Again, You may be a Blocked User... [#]")
+            print("\n\033[0;37;41m [#] Check Again, You may be a Blocked User... [#]")
             print(bar)
 
         ss+=1
-        print("\033[1;0;40m\n",str(ss), "Plz  Wait For Next round",end="")
+        print("\033[1;34;40m\n",str(ss), "Plz  Wait For Next round",end="")
         for i in range(180):
 
             pr = i/180*100
-            print("\033[1;36;40m\n>>> [#]",str(int(pr)) +"% ",end="")
+            print("\033[0;34;47m\n>>> [#]",str(int(pr)) +"% ",end="")
 
             time.sleep(0.5)
             sys.stdout.write("\033[F")
 
-    os.system('figlet FINISHED')
+    os.system('toilet Mission Complete!!!')
     again()
 
 
 def again():
-    again = input('\033[1;0;40m\nDo You want use again (y/n) - ')
+    again = input('\033[1;36;40m\nDo You want use again (y/n) - ')
     if again == "y" or again == "Y":
         main()
     elif again == "n" or again == "N":
